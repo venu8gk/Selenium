6520df6f5ca1b51fbf826fc3debef3e7b3f191bb
Venu <venu8gk@gmail.com>
2020-05-07 22:11:49 +0100

    Arch1 in BST zone

--- src/test/java/MavenJava/RestAPITest.java
+++ src/test/java/MavenJava/RestAPITest.java
@@ -9,11 +9,13 @@ public class RestAPITest {
 		System.out.println("This is API Test");
 		System.out.println("Added the GIT Demo");
 		System.out.println("Git Demo from X User");
+		System.out.println("This is Branch **********");
 	}
 	@Test()
 	public void TestAPI2()
 	{
 		System.out.println("This is API2 Test");
+		System.out.println("This is Branch ******");
 	}
 
 	
