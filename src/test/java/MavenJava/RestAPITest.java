9a34e74c35983cbd7daf2d92b33df1f93e791b42
Venu <venu8gk@gmail.com>
2020-05-07 20:14:42 +0100

    first Commit

---
+++ src/test/java/MavenJava/RestAPITest.java
@@ -0,0 +1,18 @@
+package MavenJava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	@Test()
+	public void TestAPI()
+	{
+		System.out.println("This is API Test");
+	}
+	@Test()
+	public void TestAPI2()
+	{
+		System.out.println("This is API2 Test");
+	}
+
+	
+}
---
+++ src/test/java/MavenJava/SeleniumTest.java
@@ -0,0 +1,18 @@
+package MavenJava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test()
+	public void Brouserautomation()
+	{
+		System.out.println("This is bowser Test");
+	}
+	
+	@Test()
+	public void elementSUT()
+	{
+		System.out.println("This SUT Repo");
+	}
+}
